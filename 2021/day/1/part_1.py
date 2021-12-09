463c97757faaab405f14916804e844ecb8108f51
Joseph Njeri <njeri@edi.gmbh>
2021-12-09 19:21:09 +0100

    Add some data and modify readme

--- 2021/day/1/part_1.py
+++ 2021/day/1/part_1.py
@@ -1,3 +1,26 @@
 """
 Day 1: Sonar Sweep
 """
+
+class sleight_keys:
+    """
+    we need to find slight keys:
+    day one: count the number of times
+    there is an increase in the depth
+    """
+    def __init__(self, depth_file):
+        """
+        Constructor class
+        :param depth_file: a file containing depth measurements
+        """
+        self.depth_increase_count = None
+        self.depth_file = depth_file
+
+    def count_depth_increases(self):
+        """
+        count the number times the depth increases
+        :return: int
+        """
+
+
+
